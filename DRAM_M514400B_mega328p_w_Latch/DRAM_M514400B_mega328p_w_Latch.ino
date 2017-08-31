8766306b5930e468194bbb920f7316d7c89669cc
Sin Shimozono <una.veritas@me.com>
2017-09-01 00:42:48 +0900

    dram with latch

--- DRAM_M514400B_mega328p_w_Latch/DRAM_M514400B_mega328p_w_Latch.ino
+++ DRAM_M514400B_mega328p_w_Latch/DRAM_M514400B_mega328p_w_Latch.ino
@@ -13,31 +13,23 @@ const byte DRAM_WE = 1<<5;   // PB5
 const byte DRAM_OE = 1<<4;   // PB4
 const byte DRAM_CAS = 1<<3;  // PB3
 const byte DRAM_RAS = 1<<2;  // PB2
-/*
-const byte ADDR_BUS[] = {
-  14, 15, 16, 17, 18, 19, 6, 7, 
-  8, 9,
-};
-const byte ADDR_BUS_WIDTH = 10;
-const byte DATA_IO[] = {
-  2, 3, 4, 5,
-};
-const byte DATA_IO_WIDTH = 4;
-*/
+
 #define DATA_OUT  PORTC
 #define DATA_IN   PINC
 #define DATA_DDR  DDRC
 #define DATA_MASK 0x0f
 
+// PC0 -- PC5
 #define ADDRL_PORT  PORTC
-#define ADDRM_PORT  PORTD
-#define ADDRH_PORT  PORTD
 #define ADDRL_DDR   DDRC
-#define ADDRM_DDR   DDRD
-#define ADDRH_DDR   DDRD
-// 4, 5, 6, 7
 #define ADDRL_MASK  0x3f
+// PD6, PD7
+#define ADDRM_PORT  PORTD
+#define ADDRM_DDR   DDRD
 #define ADDRM_MASK  0xc0
+// PD4, PD5
+#define ADDRH_PORT  PORTD
+#define ADDRH_DDR   DDRD
 #define ADDRH_MASK  0x30
 
 #define ADDR_LATCH_PORT  PORTB
@@ -61,29 +53,39 @@ uint8_t crc8(uint8_t inbyte, uint8_t & crc) {
 inline void addr_out(uint16_t addr) {
   // ADDRL bits are output in default
   ADDRL_DDR  |= ADDRL_MASK;
-  ADDRL_PORT = (uint8_t) addr;
+  ADDRL_PORT = (uint8_t) addr; // & ADDRL_MASK;
+  /*
   ADDRM_PORT &= ~ADDRM_MASK;
-  ADDRM_PORT |= (uint8_t) addr & ADDRM_MASK;
   ADDRH_PORT &= ~ADDRH_MASK;
+  ADDRM_PORT |= (uint8_t) addr & ADDRM_MASK;
   ADDRH_PORT |= (uint8_t)(addr>>4) & ADDRH_MASK;
-//  ADDRH_PORT |= (addr>>8 & ADDRH_MASK)<<4;
+  */
+  ADDRM_PORT &= ~(ADDRM_MASK | ADDRH_MASK);
+  ADDRM_PORT |= (uint8_t) addr & ADDRM_MASK;
+  ADDRH_PORT |= (uint8_t)(addr>>4) & ADDRH_MASK;
   ADDR_LATCH_PORT &= ~ADDR_LATCH;
   ADDR_LATCH_PORT |= ADDR_LATCH;
 }
 
 inline void data_out(uint8_t val) {
-  // ADDRL bits are output in default
+  // assuming no default pin mode
   DATA_DDR |= DATA_MASK;  
   DATA_OUT = val;
+  DRAM_WE_PORT &= ~DRAM_WE; //digitalWrite(DRAM_WE_PIN,LOW);
+  DRAM_WE_PORT |= DRAM_WE; //digitalWrite(DRAM_WE_PIN,HIGH);
 }
 
 inline uint8_t data_in() {
   uint8_t val;
   DATA_DDR &= ~DATA_MASK;
+  DRAM_OE_PORT &= ~DRAM_OE; //digitalWrite(DRAM_OE_PIN,LOW);
+  /*
   __asm__ __volatile("nop");
   val = DATA_IN;
-  DATA_DDR |= DATA_MASK;
-  return val & 0x0f;
+  */
+  DRAM_OE_PORT |= DRAM_OE; //digitalWrite(DRAM_OE_PIN,HIGH);
+  val = DATA_IN;
+  return val & DATA_MASK;
 }
 
 uint8_t dram_read(const uint32_t & addr) {
@@ -96,26 +98,26 @@ uint8_t dram_read(const uint32_t & addr) {
 
   cli();
   addr_out(row);
-  PORTB &= ~DRAM_RAS; //digitalWrite(DRAM_RAS_PIN,LOW);
+  DRAM_RAS_PORT &= ~DRAM_RAS; //digitalWrite(DRAM_RAS_PIN,LOW);
 
   // low nibble
   addr_out(col);
   PORTB &= ~DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,LOW);
-  PORTB &= ~DRAM_OE; //digitalWrite(DRAM_OE_PIN,LOW);
+  //PORTB &= ~DRAM_OE; //digitalWrite(DRAM_OE_PIN,LOW);
   data = data_in();
-  PORTB |= DRAM_OE; //digitalWrite(DRAM_OE_PIN,HIGH);
+  //PORTB |= DRAM_OE; //digitalWrite(DRAM_OE_PIN,HIGH);
   PORTB |= DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,HIGH);
 
   // high nibble
   addr_out(col | 1);
   PORTB &= ~DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,LOW);
-  PORTB &= ~DRAM_OE; //digitalWrite(DRAM_OE_PIN,LOW);
+  //PORTB &= ~DRAM_OE; //digitalWrite(DRAM_OE_PIN,LOW);
   data |= (data_in()<<4);
-  PORTB |= DRAM_OE; //digitalWrite(DRAM_OE_PIN,HIGH);
+  //PORTB |= DRAM_OE; //digitalWrite(DRAM_OE_PIN,HIGH);
   PORTB |= DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,HIGH);
 
   // 8 bits read completed
-  PORTB |= DRAM_RAS; //ådigitalWrite(DRAM_RAS_PIN,HIGH);
+  DRAM_RAS_PORT |= DRAM_RAS; //ådigitalWrite(DRAM_RAS_PIN,HIGH);
   
   sei();
   return data;  
@@ -134,15 +136,11 @@ void dram_write(const uint32_t addr, uint8_t data) {
   addr_out(col);
   PORTB &= ~DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,LOW);
   data_out(data);
-  PORTB &= ~DRAM_WE; //digitalWrite(DRAM_WE_PIN,LOW);
-  PORTB |= DRAM_WE; //digitalWrite(DRAM_WE_PIN,HIGH);
   PORTB |= DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,HIGH);
   // high nibble
   addr_out(col | 1);
   PORTB &= ~DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,LOW);
   data_out(data>>4);
-  PORTB &= ~DRAM_WE; //digitalWrite(DRAM_WE_PIN,LOW);
-  PORTB |= DRAM_WE; //digitalWrite(DRAM_WE_PIN,HIGH);
   PORTB |= DRAM_CAS; //digitalWrite(DRAM_CAS_PIN,HIGH);
   // 8 bits read completed
   PORTB |= DRAM_RAS; //digitalWrite(DRAM_RAS_PIN,HIGH);
@@ -161,8 +159,6 @@ void init_timer1_interrupt() {
   TCCR1B |= (1 << CS12);
   // enable timer compare interrupt:
   TIMSK1 |= (1 << OCIE1A);
-
-  CAS_before_RAS();
   sei();      // interrupts are also enabled here, starting refresh 
 }
 
@@ -189,8 +185,8 @@ inline void CAS_before_RAS(void) {
 
 ISR(TIMER1_COMPA_vect)
 {
-  RAS_only();
-  //CAS_before_RAS();
+  //RAS_only();
+  CAS_before_RAS();
 }
 
 long errcount = 0;
@@ -205,10 +201,7 @@ void test() {
   }
 }
 
-void setup() {
-  Serial.begin(19200);
-  Serial.println("Hi there.");
-
+void init_dram() {
   DRAM_RAS_DDR |= DRAM_RAS;
   DRAM_CAS_DDR |= DRAM_CAS;
   DRAM_OE_DDR |= DRAM_OE;
@@ -225,15 +218,15 @@ void setup() {
   ADDRH_DDR |= ADDRH_MASK;
 
   DATA_DDR |= DATA_MASK;
-/*
-  for(int i = 0; i < ADDR_BUS_WIDTH; i++) {
-    pinMode(ADDR_BUS[i], OUTPUT);
-  }
-  for(int i = 0; i < DATA_IO_WIDTH; i++) {
-    pinMode(DATA_IO[i], OUTPUT);
-  }
-  */
 
+  CAS_before_RAS();  
+}
+
+void setup() {
+  Serial.begin(19200);
+  Serial.println("Hi there.");
+
+  init_dram();
   init_timer1_interrupt();
   Serial.println("Let's go!");
 
@@ -242,42 +235,76 @@ void setup() {
 void loop() {
   uint32_t addr;
   uint8_t data, crc8val;
+  const uint16_t testBytes = 512;
+  long swatch;
   
-  addr = random(0,0x40000-2);
+  addr = random(0,0x80000);
+  randomSeed(addr);
 
-  Serial.print("from 0x");
-  Serial.println(addr, HEX);
-  Serial.print("write ");
+  Serial.print("From 0x");
+  Serial.print(addr, HEX);
+  Serial.print(", ");
 
+  Serial.print(testBytes, DEC);
+  Serial.print(" bytes write ");
   crc8val = 0;
-  for(uint32_t i = 0; i < 64; i++) {
-    data = random('0','Z');
-    Serial.print((char)data);
+  swatch = micros();
+  for(uint32_t i = 0; i < testBytes; i++) {
+    data = random(0,256);
     
     dram_write(addr+i, data);
     crc8(data, crc8val);
   }
-  dram_write(addr+64, crc8val);
-  Serial.println();
+  dram_write(addr+testBytes, crc8val);
+  swatch = micros() - swatch;
+  Serial.print("(");
+  Serial.print(swatch, DEC);
+  Serial.print("), ");
   //delay(200);
 
   // read
-  Serial.print("read  ");
+  Serial.print("read ");
   crc8val = 0;
-  for(uint32_t i = 0; i < 64; i++) {
+  swatch = micros();
+  for(uint32_t i = 0; i < testBytes; i++) {
     data = dram_read(addr+i);
     crc8(data, crc8val);
-
-    Serial.print((char)data);
   }
-  data = dram_read(addr+64);
+  data = dram_read(addr+testBytes);
+  swatch = micros() - swatch;
+  Serial.print("(");
+  Serial.print(swatch, DEC);
+  Serial.print("), ");
   crc8(data,crc8val);
-  Serial.print(": ");
   Serial.print(crc8val, HEX);
   if ( crc8val ) {
     errcount++;
     Serial.print("Error! ");
     Serial.print(errcount);
+    Serial.println();
+    randomSeed(addr);
+    for(uint32_t i = 0; i < testBytes; i++) {
+      data = random(0, 256);
+      Serial.print("0x");
+      Serial.print(data >> 4 & 0x0f, HEX);
+      Serial.print(data & 0x0f, HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+    crc8val = 0;
+    for(uint32_t i = 0; i < testBytes; i++) {
+      data = dram_read(addr+i);
+      Serial.print("0x");
+      Serial.print(data >> 4 & 0x0f, HEX);
+      Serial.print(data & 0x0f, HEX);
+      Serial.print(" ");
+      crc8(data, crc8val);
+    }
+    data = dram_read(addr+testBytes);
+    crc8(data,crc8val);
+    Serial.print(": ");
+    Serial.print(crc8val, HEX);
+    Serial.println();
   }
   Serial.println();
 
