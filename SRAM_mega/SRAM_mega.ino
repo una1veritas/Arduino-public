cd1fb56735321af8fb37f68c640c872449d2a235
Sin Shimozono <una.veritas@me.com>
2017-09-08 13:04:46 +0900

    sram_mega

--- SRAM_mega/SRAM_mega.ino
+++ SRAM_mega/SRAM_mega.ino
@@ -1,46 +1,85 @@
-#include "SRAM_mega.h"
+#include "sram.h"
 
-char text[] = "If I speak in the tongues of men or of angels, but do not have love, I am only a resounding gong or a clanging cymbal. If I have the gift of prophecy and can fathom all mysteries and all knowledge, and if I have a faith that can move mountains, but do not have love, I am nothing. If I give all I possess to the poor and give over my body to hardship that I may boast, but do not have love, I gain nothing.\n\nLove is patient, love is kind. It does not envy, it does not boast, it is not proud. It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. Love does not delight in evil but rejoices with the truth. It always protects, always trusts, always hopes, always perseveres.\n\nLove never fails. But where there are prophecies, they will cease; where there are tongues, they will be stilled; where there is knowledge, it will pass away. For we know in part and we prophesy in part, but when completeness comes, what is in part disappears. When I was a child, I talked like a child, I thought like a child, I reasoned like a child. When I became a man, I put the ways of childhood behind me. For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known.\n\nAnd now these three remain: faith, hope and love. But the greatest of these is love.\n";
-SRAM sram;
+/* sram_avr.c functions */
 
-long addr = 0;
-uint16_t length = strlen(text);
+void sram_init() {
+  ADDRL_DIR = 0xff;
+  ADDRH_DIR = 0xff;
+  ADDRX_DIR |= ADDRX_MASK;
+  DATA_DIR = 0x00;
+  CONTROL_DIR |= ( SRAM_CS | SRAM_WE | SRAM_OE ); //| LATCH_L );
+  CONTROL |= ( SRAM_CS | SRAM_WE | SRAM_OE ); //| LATCH_L );
+}
+
+inline void addr_out(uint32_t addr) {
+  ADDRL = addr & 0xff;
+  addr >>= 8;
+  ADDRH = addr & 0xff;
+  addr >>= 8;
+  ADDRX &= ~ADDRX_MASK;
+  ADDRX |= addr & ADDRX_MASK;
+}
+
+
+unsigned char sram_read(unsigned short addr) {
+  unsigned char val;
+  CONTROL &= ~SRAM_CS;
+  addr_out(addr);  // A16 is always 0
+  DATA_DIR = 0x00;
+  CONTROL &= ~SRAM_OE;
+  CONTROL |= ~SRAM_OE;
+  val = DATA_IN;
+  CONTROL |= SRAM_CS;
+  return val;
+}
+
+void sram_write(unsigned short addr, unsigned char data) {
+  CONTROL &= ~SRAM_CS;
+  addr_out(addr);  // A16 is always 0
+  DATA_DIR = 0xff;
+  DATA_OUT = data;
+  CONTROL &= ~SRAM_WE;
+  CONTROL |= SRAM_WE;
+  CONTROL |= SRAM_CS;
+}
+/* sram_avr.c end */
+
+const char text[] = "If I speak in the tongues of men or of angels, but do not have love, I am only a resounding gong or a clanging cymbal. If I have the gift of prophecy and can fathom all mysteries and all knowledge, and if I have a faith that can move mountains, but do not have love, I am nothing. If I give all I possess to the poor and give over my body to hardship that I may boast, but do not have love, I gain nothing.\n\nLove is patient, love is kind. It does not envy, it does not boast, it is not proud. It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. Love does not delight in evil but rejoices with the truth. It always protects, always trusts, always hopes, always perseveres.\n\nLove never fails. But where there are prophecies, they will cease; where there are tongues, they will be stilled; where there is knowledge, it will pass away. For we know in part and we prophesy in part, but when completeness comes, what is in part disappears. When I was a child, I talked like a child, I thought like a child, I reasoned like a child. When I became a man, I put the ways of childhood behind me. For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known.\n\nAnd now these three remain: faith, hope and love. But the greatest of these is love.\n";
+long addr = 0; 
 
 void setup() {
-  Serial.begin(38400);
-  Serial.println("Started.");
-  
-	sram.begin();
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  Serial.println("Hi, there.");
+  Serial.println("Let's start.");
 
+  sram_init();
+  
 }
 
 void loop() {
-  byte val;
-  uint16_t offset = random(0, length - 128);
-
+  // put your main code here, to run repeatedly:
+  uint8_t val;
   addr = random(0, 0x1ffff);
-  Serial.print("base addr: ");
+  
   Serial.println(addr, HEX);
-  Serial.print("write: \t");
-  sram.enable();
-  for(int i = 0; i < 128; i++) {
-    val = text[offset+i]; //random(' ', 'Z');
-    Serial.print((char)val);
-    //Serial.print(", ");
-    sram.bank(addr>>16 & 1);
-    sram.write(addr+i, val);
+  for(uint16_t i = 0; i < 16; i++) {
+    val = random(0, 256);
+    Serial.print(val, HEX);
+    Serial.print(' ');
+    sram_write(addr+i, val);
+    CONTROL |= SRAM_CS;
   }
   Serial.println();
-  Serial.print("read: \t");
-  for(int i = 0; i < 128 ; i++) {
-    val = sram.read(addr+i);
-    Serial.print((char)val);
-    //Serial.print(", ");
-  }
-  Serial.println();
-  sram.disable();
-  Serial.println();
+  delay(500);
 
+  for(uint16_t i = 0; i < 16; i++) {
+    val = sram_read(addr+i);
+    Serial.print(val, HEX);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
   delay(500);
 }
 
