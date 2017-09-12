3f5614cb6ee3824e5ad56ea659e2f24b21d96599
Sin Shimozono <una.veritas@me.com>
2017-09-12 12:51:13 +0900

    ale by hc573

--- SRAM_mega/sram.h
+++ SRAM_mega/sram.h
@@ -3,24 +3,32 @@
 /* SRAM I/F Port/Pin definitions */
 #define ADDRL_DIR DDRA
 #define ADDRL PORTA
+#define ADDRL_MASK 0xff
 #define ADDRH_DIR DDRC
 #define ADDRH PORTC
-#define ADDRX_DIR DDRL
-#define ADDRX PORTL
-#define ADDRX_MASK (1<<0)
+#define ADDRH_MASK 0xff
+#define ADDRX_DIR DDRD
+#define ADDRX PORTD
+#define ADDRX_MASK (1<<PD7)
 
-#define DATA_OUT  PORTF
-#define DATA_IN  PINF
-#define DATA_DIR DDRF
+#define DATA_DIR DDRA
+#define DATA_OUT  PORTA
+#define DATA_IN  PINA
 
-#define CONTROL PORTL
-#define CONTROL_DIR DDRL
-#define SRAM_CS (1<<1)
-#define SRAM_OE (1<<2)
-#define SRAM_WE (1<<3)
-//#define LATCH_L (1<<7)
+#define CONTROL_DIR DDRG
+#define CONTROL PORTG
+#define SRAM_OE (1<<PG1)
+#define SRAM_WE (1<<PG0)
+#define SRAM_ALE (1<<PG2)
+#define CONTROL_CS_DIR DDRL
+#define CONTROL_CS PORTL
+#define SRAM_CS (1<<PL6)
+
+#define bitset(port, bv) (port) |= (bv)
+#define bitclear(port, bv) (port) &= ~(bv)
 
 void sram_init(void);
-unsigned char sram_read(unsigned short addr);
-void sram_write(unsigned short addr, unsigned char data);
+uint8_t sram_read(uint8_t addr);
+void sram_write(uint16_t addr, uint8_t data);
+void sram_bank(uint8_t bk);
 
