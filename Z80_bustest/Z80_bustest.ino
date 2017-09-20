ddfb268c2facc80e9fbe454984cef227b8200189
Sin Shimozono <una.veritas@me.com>
2017-09-20 12:42:42 +0900

    z80

--- Z80_bustest/Z80_bustest.ino
+++ Z80_bustest/Z80_bustest.ino
@@ -5,10 +5,13 @@
 #define Z80_RESET_PIN 10
 #define Z80_BUSREQ_PIN  9
 #define Z80_WAIT_PIN  8
-#define Z80_BUSACK    7
+#define Z80_BUSACK_PIN    7
 #define Z80_WR_PIN    41
 #define Z80_RD_PIN    40
 
+#define Z80_INT_PIN   62
+#define Z80_NMI_PIN   63
+#define Z80_HALT_PIN  64
 #define Z80_MREQ_PIN  5
 #define Z80_IORQ_PIN  6
 
@@ -32,52 +35,105 @@ void init_Timer1(uint8_t presc, uint16_t top) {
 }
 
 uint8_t mem[] = {
-  0x3e, 0x01, 0x3c, 0x3c, 0x47, 0xd3, 0x01, 0xdb, 0x01, 0x18, 0xf7,
-  0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 0x00,
   };
 const uint8_t memsize = sizeof mem;
 
+bool clkpulse = HIGH;
+uint16_t opcycle = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(19200);
+  while(!Serial);
   Serial.println("Hi there.");
-
+  
   DDRF = 0xff;
   PORTF = 0;
   DDRA = 0x00;
   DDRC = 0x00;
-  
-  init_Timer1(5, 3072);
-  
-  pinMode(Z80_M1_PIN, INPUT);
-  digitalWrite(Z80_M1_PIN, 0xff);
+    
   pinMode(Z80_MREQ_PIN, INPUT);
+  pinMode(Z80_IORQ_PIN, INPUT);
   pinMode(Z80_RD_PIN, INPUT);
+  pinMode(Z80_WR_PIN, INPUT);
+
+  pinMode(Z80_M1_PIN, INPUT);
+  pinMode(Z80_BUSACK_PIN, INPUT);
+  pinMode(Z80_HALT_PIN, INPUT);
+
   
   pinMode(Z80_RESET_PIN, OUTPUT);
   digitalWrite(Z80_RESET_PIN, HIGH);
+  pinMode(Z80_BUSREQ_PIN, OUTPUT);
+  digitalWrite(Z80_BUSREQ_PIN, HIGH);
+  pinMode(Z80_WAIT_PIN, OUTPUT);
+  digitalWrite(Z80_WAIT_PIN, HIGH);
+  pinMode(Z80_NMI_PIN, OUTPUT);
+  digitalWrite(Z80_NMI_PIN, HIGH);
+  pinMode(Z80_INT_PIN, OUTPUT);
+  digitalWrite(Z80_INT_PIN, HIGH);
+  
+  init_Timer1(5, 4000);
   delay(1000);
   digitalWrite(Z80_RESET_PIN, LOW);
-  delay(500);
+  delay(1000);
   digitalWrite(Z80_RESET_PIN, HIGH);
 
 }
 
 void loop() {
+  enum edges{
+    falling,
+    bottom,
+    rising,
+    top,
+  };
+  edges edge = bottom;
   uint16_t addr;
-  // put your main code here, to run repeatedly:
-  if ( !digitalRead(Z80_MREQ_PIN) && !digitalRead(Z80_RD_PIN) ) {
-    if ( !digitalRead(Z80_M1_PIN) ) 
-      Serial.print("M1 ");
-    addr = (((uint16_t)PINC<<8) | PINA);
-    Serial.print(addr, HEX); 
-    Serial.print(": "); 
-    DDRF = 0xff;
-    PORTF = mem[addr & (sizeof mem - 1)];
-    Serial.print(PORTF, HEX);
-    while ( !digitalRead(Z80_RD_PIN) );
-    Serial.println(" -> RD up");
-  }
 
+  if ( digitalRead(Z80_CLK_PIN) == LOW ) {
+    if ( clkpulse == HIGH ) {
+      edge = falling;
+      clkpulse = LOW;
+    } else {
+      edge = bottom;
+    }
+  } else {
+    if ( clkpulse == HIGH ) {
+      edge = top;
+    } else {
+      clkpulse = HIGH;
+      edge = rising;
+    }
+  }
+  
+  if ( edge == falling ) {
+    addr = PINA;
+    addr = ((uint16_t) PINC)<<8;
+    Serial.print(addr, HEX);
+    opcycle++;
+    if ( !digitalRead(Z80_M1_PIN) && !digitalRead(Z80_MREQ_PIN) && !digitalRead(Z80_RD_PIN) ) {
+      if ( opcycle == 1 ) {
+        Serial.print(" (M1) ");
+        Serial.println();
+      } else {
+        Serial.println();
+      }
+    } if ( !digitalRead(Z80_MREQ_PIN) && !digitalRead(Z80_RD_PIN) ) {
+        Serial.print(" (RD) ");
+        Serial.println();
+        opcycle = 0;
+    } else if ( !digitalRead(Z80_MREQ_PIN) && !digitalRead(Z80_WR_PIN) ) {
+        Serial.print(" (WR) ");
+        Serial.println();
+        opcycle = 0;
+    } else {
+        Serial.println();
+        opcycle = 0;
+    }
+  }
+  
 }
