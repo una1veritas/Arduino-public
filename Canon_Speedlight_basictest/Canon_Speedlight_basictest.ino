446747511047c6a4cab9e6eb0700e0c1c75e724f
Sin Shimozono <una.veritas@me.com>
2019-03-29 21:53:00 +0900

    Timer1

--- Canon_Speedlight_basictest/Canon_Speedlight_basictest.ino
+++ Canon_Speedlight_basictest/Canon_Speedlight_basictest.ino
@@ -1,3 +1,6 @@
+
+#include <Timer1.h>
+
 const int PIN_XBAR = 2;
 const int PIN_Y = 6;
 const int PIN_Q = 7;
@@ -14,37 +17,38 @@ void show_state(int stat) {
 }
 
 volatile int state;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(PIN_XBAR, INPUT);
   pinMode(PIN_Y, OUTPUT); digitalWrite(PIN_Y, LOW);
   pinMode(PIN_Q, INPUT);
-  pinMode(PIN_LED, OUTPUT); digitalWrite(PIN_LED, HIGH);
+  pinMode(PIN_LED, OUTPUT); digitalWrite(PIN_LED, LOW);
 
   attachInterrupt(0, X_start, RISING);
 
+  Timer1.mode_ctc(200);
+  
   Serial.begin(19200);
   state = (PIND & (1<<PIN_XBAR | 1<<PIN_Q));
   show_state(state);
 }
 
 void X_start() {
-  for(int i = 0; i < 100; ++i) {
-    _delay_ms(10);
-  }
+  Timer1.start(Timer1.CS_DIV256);
   digitalWrite(PIN_Y, HIGH);
+  digitalWrite(PIN_LED, HIGH);  
+}
+
+ISR(TIMER1_COMPA_vect) {
+  cli();
+  Timer1.stop();
+  digitalWrite(PIN_Y, LOW);
+  digitalWrite(PIN_LED, LOW);  
+  sei();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  int newstate = (PIND & (1<<PIN_XBAR | 1<<PIN_Q));
-  if ( state != newstate ) {
-    if (  (state>>PIN_XBAR & 1) == 1 && (newstate>>PIN_XBAR & 1) == 0 ) {
-      digitalWrite(PIN_Y, LOW);
-    }
-    state = newstate;
-    show_state(state);
-  }
     
 }
---
+++ libraries/Timer1/Timer1.cpp
@@ -0,0 +1,10 @@
+/*
+ * Timer1.cpp
+ *
+ *  Created on: 2012/02/18
+ *      Author: sin
+ */
+
+#include <Timer1.h>
+
+atmega328_Timer1 Timer1;
