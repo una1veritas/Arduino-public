31a1c8f291382841aed1ba29e0cb5d10c61de5b6
Sin Shimozono <una.veritas@me.com>
2026-05-07 23:31:56 +0900

    Revert "uni_programmer"
    This reverts commit e19e81297fe5347cfa981da6cdebc8e520d2810d.

--- UniProgrammer/Debug/subdir.mk
+++ UniProgrammer/Debug/subdir.mk
@@ -8,18 +8,32 @@ CPP_SRCS += \
 ../ihex_processor.cpp \
 ../srec_processor.cpp 
 
+INO_SRCS += \
+../UniProgrammer.ino 
+
 CPP_DEPS += \
 ./common.d \
 ./ihex_processor.d \
 ./srec_processor.d 
 
+INO_DEPS += \
+./UniProgrammer.d 
+
 OBJS += \
+./UniProgrammer.o \
 ./common.o \
 ./ihex_processor.o \
 ./srec_processor.o 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.ino subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	avr-g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 %.o: ../%.cpp subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
@@ -31,7 +45,7 @@ OBJS += \
 clean: clean--2e-
 
 clean--2e-:
-	-$(RM) ./common.d ./common.o ./ihex_processor.d ./ihex_processor.o ./srec_processor.d ./srec_processor.o
+	-$(RM) ./UniProgrammer.d ./UniProgrammer.o ./common.d ./common.o ./ihex_processor.d ./ihex_processor.o ./srec_processor.d ./srec_processor.o
 
 .PHONY: clean--2e-
 
--- UniProgrammer/UniProgrammer.ino
+++ UniProgrammer/UniProgrammer.ino
@@ -21,10 +21,8 @@ Replace yourfile.hex with your Intel HEX filename
 
 #include <SPI.h>
 #include <SPISRAM.h>
-//#include <MCP23S08.h>
-//#include <MCP23S17.h>
-
-#include "uni_programmer.h"
+#include <MCP23S08.h>
+#include <MCP23S17.h>
 
 #include "common.h"
 
