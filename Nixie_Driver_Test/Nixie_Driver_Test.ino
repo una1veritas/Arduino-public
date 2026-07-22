b02ef18e50a6c9f0c6bd853baff300ec739466b1
Sin Shimozono <una.veritas@me.com>
2026-07-23 01:38:51 +0900

    Nixie Driver Test

--- Nixie_Driver_Test/Nixie_Driver_Test.ino
+++ Nixie_Driver_Test/Nixie_Driver_Test.ino
@@ -1,7 +1,8 @@
 
-constexpr int cathode[] = { 3, 4, 5, 6, 0};
+constexpr int cathode[] = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0};
 int cath_num;
 int cath_id;
+long last_millis = 0;
 void setup() {
   // put your setup code here, to run once:
   int i;
@@ -12,14 +13,18 @@ void setup() {
   cath_num = i;
   cath_id = 0;
   //Serial.begin(115200);
+
+  last_millis = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.print(cath_id); Serial.print(", "); Serial.println(cathode[cath_id]);
-  digitalWrite(cathode[cath_id], HIGH);
-  delay(2000);
-  digitalWrite(cathode[cath_id], LOW);
-  //delay(250);
-  cath_id = (cath_id + 1) % cath_num;
+  if ( millis() - last_millis >= 1000 ) {
+    last_millis = millis();
+    digitalWrite(cathode[cath_id], LOW);
+    //delay(20);
+    cath_id = (cath_id + 1) % cath_num;
+    digitalWrite(cathode[cath_id], HIGH);
+  }
 }
