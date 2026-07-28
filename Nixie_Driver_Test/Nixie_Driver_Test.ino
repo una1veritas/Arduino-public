91fab28ad227f2dea12e52e2c7c76d6f0df0718a
Sin Shimozono <una.veritas@me.com>
2026-07-28 11:49:13 +0900

    nixie

--- Nixie_Driver_Test/Nixie_Driver_Test.ino
+++ Nixie_Driver_Test/Nixie_Driver_Test.ino
@@ -1,30 +1,48 @@
 
-constexpr int cathode[] = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0};
-int cath_num;
-int cath_id;
-long last_millis = 0;
-void setup() {
-  // put your setup code here, to run once:
-  int i;
-  for(i = 0; cathode[i] != 0; ++i) {
-    digitalWrite(cathode[i], LOW);
-    pinMode(cathode[i], OUTPUT);
-  }
-  cath_num = i;
-  cath_id = 0;
-  //Serial.begin(115200);
+enum CS1x_CLK_SEL_BITS {
+  // without shift
+  CLK_STOP = 0,
+  CLK_T1S_1 = 1,
+  CLK_T1S_8 = 2,
+};
 
-  last_millis = millis();
+// global 
+constexpr static uint8_t OC1A_PWM_PIN = 9;
+
+inline void TIMER1_CLK_STOP() { TCCR1B = (3 << WGM12) | CLK_STOP; }
+inline void TIMER1_CLK_DIV8() { TCCR1B = (3 << WGM12) | CLK_T1S_8; }
+inline bool TIMER1_CLK_DISABLED() { return (TCCR1B & 0x07) == 0; }
+
+long sec;
+
+void setup() {
+  // Setup Timer/Counter1 in Fast PWM mode, 25kHz 
+  
+  // Set Pin 9/PB1/OC1A as an output
+  pinMode(OC1A_PWM_PIN, OUTPUT); 
+  //
+  // Fast PWM Mode 
+  TCCR1A = (2 << COM1A0) | (2 << WGM10);
+  TCCR1B = (3 << WGM12) | CLK_T1S_8;
+  // 
+  // Set the TOP limit for 25 kHz
+  const unsigned int PERIOD = 83;
+  ICR1 = PERIOD; //CLKDIV8, 79 -> 25kHz
+  //
+  // Set Duty Cycle (0 to 127)
+  OCR1A = PERIOD*3/4 ; // 470uH with three stage CW -> 400V
+
+  Serial.begin(115200);
+
+  sec = (millis() / 1000) % 10;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.print(cath_id); Serial.print(", "); Serial.println(cathode[cath_id]);
-  if ( millis() - last_millis >= 1000 ) {
-    last_millis = millis();
-    digitalWrite(cathode[cath_id], LOW);
-    //delay(20);
-    cath_id = (cath_id + 1) % cath_num;
-    digitalWrite(cathode[cath_id], HIGH);
+  int t = (millis() / 1000) % 10;
+  if ( t != sec) {
+    sec = t;
+    //Serial.print(sec); Serial.println();
+    PORTB &= 0xf0;
+    PORTB |= sec & 0x0f;
   }
 }
